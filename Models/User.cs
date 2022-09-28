94af3d980c0a155c50081f3a45dc084810deb866
Jostautas <jostautas.sakas@gmail.com>
2022-09-28 16:48:38 +0300

    deleted unused model

--- Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GreenLocator.Models
-{
-    public class User
-    {
-        public string City { get; set; } 
-        public string Adress { get; set; }
-        public int House { get; set; }
-    }
-}
