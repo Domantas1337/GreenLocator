7f12f6cdc9c1634e0531fe791efbe262fdc0d960
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2022-10-09 22:26:16 +0300

    Started setuping and configuring SignalR in our project.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using GreenLocator.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using GreenLocator.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -37,5 +39,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
