cdc6c0cbb5714a6abe1c1fa697a8e50883b591a4
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2022-10-26 11:14:05 +0300

    Created delegate class

---
+++ Pages/Delegates.cs
@@ -0,0 +1,8 @@
+﻿namespace GreenLocator.Pages
+{
+    public class Delegates
+    {
+        public delegate string StringToString(string x);
+        public delegate int IntIntToInt(int x, int y);
+    }
+}
