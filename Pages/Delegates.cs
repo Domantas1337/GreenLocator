018c04b83716a5545e24c7a45778700223f183d3
Dominykas <113127442+Dom1nyk4s@users.noreply.github.com>
2022-10-26 13:30:48 +0300

    Declarations of delegate in Enterinfo.cshtml and Delegates.cs

--- Pages/Delegates.cs
+++ Pages/Delegates.cs
@@ -4,5 +4,7 @@
     {
         public delegate string StringToString(string x);
         public delegate int IntIntToInt(int x, int y);
+
+        //StringToString dValues = Pages_EnterInfo.Values;
     }
 }
