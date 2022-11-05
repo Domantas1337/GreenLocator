82ad7b7d60e15dfb845140675dba8a36cadd13f6
Jostautas <jostautas.sakas@gmail.com>
2022-11-05 20:31:14 +0200

    atempt to test OnGet() from Main model

--- GLTests/UnitTest1.cs
+++ GLTests/UnitTest1.cs
@@ -5,12 +5,13 @@ using Moq;
 using AutoFixture;
 using AutoFixture.Xunit2;
 using AutoFixture.AutoMoq;
+using GreenLocator.Pages;
+using NuGet.Frameworks;
 
 namespace GLTests
 {
     public class UnitTest1
     {
-
         [Fact]
         public void ConnectionToDBValid()
         {
@@ -27,5 +28,22 @@ namespace GLTests
             Assert.True(context.Database.CanConnect());
         }
 
+        [Theory, AutoData]
+        public void OnGet_NullArg(IFixture fixture)
+        {
+            fixture.Customize(new AutoMoqCustomization());
+            var context = fixture.Create<GreenLocatorDBContext>();
+
+            var sut = fixture.Create<MainModel>();  // sut - system under test
+
+            var user = fixture.Create<AspNetUser>();
+            user.City = null;
+            user.Street = null;
+            user.House = null;
+
+            //Assert.Throws<ArgumentNullException>( () => sut.OnGet() );
+            Assert.Equal(user.City, null);
+
+        }
     }
 }
