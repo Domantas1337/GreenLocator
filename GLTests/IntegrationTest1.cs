995b9fb749718651b1c9835f29ec45f7fbd7bb21
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2022-11-22 17:36:28 +0200

    deleted unnecessary comments

--- GLTests/IntegrationTest1.cs
+++ GLTests/IntegrationTest1.cs
@@ -27,14 +27,14 @@ namespace GLTests
         //[InlineData("/Main")]
         public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
         {
-            // Arrange
+            
             var client = _factory.CreateClient();
 
-            // Act
+            
             var response = await client.GetAsync(url);
 
-            // Assert
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            
+            response.EnsureSuccessStatusCode();
             Assert.Equal("text/html; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
