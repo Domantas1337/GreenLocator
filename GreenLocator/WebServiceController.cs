cf686cc63dc4f30a818e27c47de0dfeb258e76b5
Jostautas <jostautas.sakas@gmail.com>
2022-12-20 14:11:57 +0200

    calculates how much power and money user has saved

--- GreenLocator/WebServiceController.cs
+++ GreenLocator/WebServiceController.cs
@@ -12,17 +12,26 @@ namespace GreenLocator;
 [ApiController]
 public class WebServiceController : Controller
 {
+    private readonly List<ElectricityPrice> AllPrices = new List<ElectricityPrice>
+    {
+        new ElectricityPrice { Country = "Austria", Price = 400 },
+        new ElectricityPrice { Country = "Belgium", Price = 403.65 },
+        new ElectricityPrice { Country = "Bulgaria", Price = 396.35},
+        new ElectricityPrice { Country = "Lithuania", Price = 384.40}
+    };
+
     [HttpGet]
     public ActionResult<IEnumerable<ElectricityPrice>> GetAll()
     {
-        return new[]
-        {
-            new ElectricityPrice { Country = "Austria", Price = 400 },
-            new ElectricityPrice { Country = "Belgium", Price = 403.65 },
-            new ElectricityPrice { Country = "Bulgaria", Price = 396.35},
-            new ElectricityPrice { Country = "Lithuania", Price = 384.40}
-        };
+        return AllPrices;
     }
+
+    [HttpGet("{country}")]
+    public ActionResult<ElectricityPrice> GetPrice(string country)
+    {
+        return AllPrices.SingleOrDefault(pr => pr.Country!.ToLower() == country.ToLower())!;
+    }
+
 }
 
 public class ElectricityPrice
